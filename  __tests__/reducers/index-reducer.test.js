d6ace4df7ef940017fcbae77daa140e3c41be837
Sailor Winkelman <sailorwinkelman@gmail.com>
2018-03-07 17:36:38 -0800

    fix root reducer issue by passing default state into test and removing object brackets from test expect

---  __tests__/reducers/index-reducer.test.js
+++  __tests__/reducers/index-reducer.test.js
@@ -3,37 +3,16 @@ import userReducer from './../../src/reducers/user-reducer';
 import operatorReducer from './../../src/reducers/operator-reducer';
 import sessionReducer from './../../src/reducers/session-reducer';
 import { createStore } from 'redux';
+import { defaultState } from './../../src/defaultState';
 
 let store = createStore(rootReducer);
 
 describe('rootReducer', () => {
 
   test('Should return default state if no action is recognized', () => {
-    expect(rootReducer({}, { type: null })).toEqual({
-      user: {
-        id: null,
-        isOperator: false,
-        isConnected: false,
-      },
-      operatorInfo: {
-        id: null,
-        avatar: null,
-        name: null,
-        location: null,
-      },
-      session: {
-        id: null,
-        timeRequested: null,
-        timeStarted: null,
-        timeClosed: null,
-        operatorId: null,
-        messages: [
-        ],
-        notes: ''
-      }
-    });
+    expect(rootReducer(defaultState, { type: null })).toEqual(defaultState);
   });
-
+'';
 
 
 });
