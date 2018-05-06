c81b401e35556061243777a7faee7c47a18350bc
Sailor Winkelman <sailor.winkelman@modop.com>
2018-05-06 14:18:50 -0700

    Resolve issue with start time in session reducer

--- src/reducers/session-reducer.js
+++ src/reducers/session-reducer.js
@@ -11,20 +11,21 @@ let newTime;
 const {firebaseConfig, types} = constants;
 
 export default (state = defaultState.session, action) => {
-  switch (action.type) {
-    case types.ADD_MESSAGE:
-		const { timeStamp, isOp, string, notes} = action;
-      newMessage = {
+    const { timeStamp, isOp, string, notes } = action;
+    newMessage = {
         timeStamp: timeStamp,
         isOp: isOp,
         string: string
-      };
-      messages = state.messages.slice();
-      messages.push(newMessage);
-      newState = Object.assign({}, state, {
-        messages: messages
-      });
-      return newState;
+    };
+  switch (action.type) {
+        case types.ADD_MESSAGE:
+          messages = state.messages.slice();
+          messages.push(newMessage);
+          newState = Object.assign({}, state, {
+            messages: messages
+          });
+
+          return newState;
 
 		case types.ADD_NOTES:
 			newNotes = notes;
@@ -37,9 +38,8 @@ export default (state = defaultState.session, action) => {
 			return newState;
 
 		case types.END_SESSION:
-			const {timeStarted} = action;
 			newTime = moment(Date.now()).format('MMMM Do YYYY, h:mm:ss a');
-			newState = Object.assign({}, state, {timeClosed:newTime, timeStarted: timeStarted});
+			newState = Object.assign({}, state, {timeClosed:newTime});
 			return newState;
 
     default:
